b650f12d468cdb8d1797c68f1ee86543010301d3
gaachi997 <menongayathri997@gmail.com>
2016-10-18 17:10:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-one'),function (req,res) {
+app.get('/Article-one',function (req,res) {
     res.send('/article one will be served here');
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
